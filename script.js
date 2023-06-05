6bcf0d20c4ef542d1a23c16065cf93a38575cc0b
Shreejal Maharjan <shreejal27@gmail.com>
2023-06-05 22:29:11 +0545

    completed but not what i imagined

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
-function updateClock() {
+
     const hand = document.querySelector('.hand');
     const currentDate = new Date();
-    // const hours = currentDate.getHours();
-    // const minutes = currentDate.getMinutes();
-    // const seconds = currentDate.getSeconds();
-    
-  
-    hand.style.transform = `translateX(-50%) translateY(-100%) rotate(51.43deg)`;
-  }
-  
-  // Call the updateClock function every second
-  setInterval(updateClock, 1000);
+    const options = { weekday: 'long' };
+    const day = currentDate.toLocaleString('en-US', options);
+
+    switch (day) {
+      case 'Sunday':
+        var degree = 0;
+        break;
+      case 'Monday':
+        var degree = 51.43;
+        break;
+      case 'Tuesday':
+        var degree = 102.86;
+        break;
+      case 'Wednesday':
+        var degree = 154.29;
+        break;
+      case 'Thursday':
+        var degree = 205.72;
+        break;
+      case 'Friday':
+        var degree = 257.15;
+        break;
+      case 'Saturday':
+        var degree = 308.58;
+        break;
+      default:
+        var degree = 0;
+        break;
+    }
+    hand.style.transform = `translateX(-50%) translateY(-100%) rotate(${degree}deg)`;
   
